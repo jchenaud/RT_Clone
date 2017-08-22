748bcc0d0562836644d25bd390f726a5106a8dbb
piquerue <pierrequeruel@gmail.com>
2017-08-22 18:16:55 +0200

    Adding new define on draw pave

--- src/draw_pave.c
+++ src/draw_pave.c
@@ -6,7 +6,7 @@
 /*   By: piquerue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/22 17:23:26 by piquerue          #+#    #+#             */
-/*   Updated: 2017/08/22 17:49:29 by piquerue         ###   ########.fr       */
+/*   Updated: 2017/08/22 18:16:35 by piquerue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # define get_hit_maxx(x) x->hitbox.r_max.x
 # define get_hit_maxy(x) x->hitbox.r_max.y
 # define get_hit_maxz(x) x->hitbox.r_max.z
+# define scalars_vector(x, y) x.x * y.x + x.y * y.y + x.z + x.z
+# define get_norm(x) sqrt(x.x * x.x + x.y * x.y + x.z * x.z)
+# define get_angle(x, y) scalar_vectors(x, y) / get_norm(x) * get_norm(y)
+# define get_dist(x, y) sqrt(pow(x.x - y.x, 2) + pow(x.y - y.y, 2) + pow(x.z - y.z, 2)
+
+
 
 int		hit_pave(t_obj *obj, cl_float3 ray)
 {
@@ -60,4 +66,9 @@ int		hit_forme(t_obj *obj, cl_float3 ray)
 		return (hit_pave(obj, ray));
 	else
 		return (0);
+}
+
+cl_char4	color_light(t_list *light, t_cam cam)
+{
+
 }
